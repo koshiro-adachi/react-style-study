644762b3575c58c29d5e39f59f62678463e935f6
koshiro-adachi <107029087+koshiro-adachi@users.noreply.github.com>
2022-07-12 21:01:56 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { CssModules } from "./compornents/CssModules";
 import { InlineStyle } from "./compornents/InlineStyle";
 import { StyledJsx } from "./compornents/StyledJsx";
+import { StyledComponents } from "./compornents/StyledComponents";
+import { Emotion } from "./compornents/Emotion.jsx";
 import "./styles.css";
 
 export default function App() {
@@ -9,6 +11,8 @@ export default function App() {
       <InlineStyle />
       <CssModules />
       <StyledJsx />
+      <StyledComponents />
+      <Emotion />
     </div>
   );
 }
